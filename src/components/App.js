9abd8cd6e3f50a3fc86980d24253649a2f8e4cb9
aFarhin <ahmeda.farhin786@gmail.com>
2023-05-17 14:50:31 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  let [count, setCount]= useState(1)
+  let [count, setCount]= useState(0)
 
   function counter(e){
     setCount(count+1)
